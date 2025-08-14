153e23a15ca1300c69c2bade027da8c80b08cc9e
maxnoelp2 <max@surfgreen.dev>
2025-08-14 11:29:25 +0200

    initial commit

---
+++ coolifytemplate/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from coolifytemplate.users.models import User
+from coolifytemplate.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
